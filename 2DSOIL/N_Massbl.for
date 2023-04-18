fabdd837fec6513a1e19f5a3b793b6512de5bbc3
ARS-CSGCL-DT <dennis.timlin@ars.usda.gov>
2023-04-18 15:50:02 -0400

    added mulch decomposition and CO2-O2 dynamics to 2dsoil

--- 2DSOIL/N_Massbl.for
+++ 2DSOIL/N_Massbl.for
@@ -4,6 +4,7 @@ Cdt N in 2DSOIL is nitrate (NO3) and the units are ug per cm3 or milligram/liter
         include 'public.ins'
         include 'nitvar.ins'
         include 'puplant.ins'
+        Include 'PuSurface.ins'
         Dimension Bi(3),Ci(3)
         Character*10 Date
         Real AE,Profile_N,Manure_N,Litter_N,
@@ -11,29 +12,54 @@ Cdt N in 2DSOIL is nitrate (NO3) and the units are ug per cm3 or milligram/liter
      &    NO3_Mean,NhumusMean,NLitterMean,NManureMean,NAmmoniaMean,All_N
         Integer ModNum
 C variables to hold mulch C and N totals
-        Real humusC, litterC, manureC, 
-     &        CHumusMean,CLitterMean, CManureMean,
-     &        CsinkHuMean, CsinkLiMean,CsinkMaMean 
-        
-        common /N_BAL/ModNum,CFlux,CFluxPrevious,Rflux
+        Real t_CARB, t_CELL,t_LIGN,t_CARB_N,t_CELL_N, t_LIGN_N,
+     &    t_ResidueMulch, t_ResidueMulch_N, t_ResidueMulch_CNR,
+     &    t_Mulch_Thick,
+     &    humusC, litterC, manureC, 
+     &    All_C, CHumusMean,CLitterMean, CManureMean
+     
+        common /N_BAL/ModNum,CFlux,CFluxPrevious,C_RespirationOM,
+     !    C_RespirationRoot
 
       If (lInput.eq.1) then
-        open(91,file=MassBalanceFileOut,status='unknown',recl=270)
-        write(91,'(14A12)') 'time,','Date,','Min_N,','Humus_N,',
+        open(91,file=MassBalanceFileOut,status='unknown',recl=520)
+        write(91,8) 'Date_time,','Date,','Min_N,','Humus_N,',
      !     'Humus_C,','Manure_N,','Manure_C,','Litter_N,',
-     !     'Litter_C,','Ammon_N,','All_N,','water,','Denitr,',
-     !     'CFlux'
-        
-   !      ,'RFlux','SinkHu_C','Sinkli_C','SinkMa_C'
+     !     'Litter_C,','OM_CO2_C,','Root_CO2_C,','Ammon_N,','All_N,',
+     !     'All_C,','water,','Denitr,','CFlux,',
+     !     'Mul_CARB_N,','Mul_CELL_N,','Mul_LIGN_N,',
+     !     'Tot_res_N,',
+     !     'Mul_Thick,', 'Mul_Mass,', 'Mul_CNR,'
 
+
+8     Format (1x,A10,T20,A5,T40,A6,T60,A8,
+     &  	  T80,A8,T104,A9,T124,A9,T144,A9,
+     &        T164,A9,T186,A9,T209,A11,T229,A8,T249,A6, 
+     &        T271,A6,T293,A7,T313,A7, 
+     &        T333,A6,T353,A11,T376,A11, 
+     &        T396,A11,
+     &        T418,A10,T438,A10,T458,A9,T482,A8)	 
+ 
       NumMod=NumMod+1
 	ModNum=NumMod
 	tNext(ModNum)=time
 	CFlux=0
 	CFluxPrevious=0
-      Rflux=0
+
+      t_CARB=0.0D0
+      t_CELL=0.0D0
+      t_LIGN=0.0D0
+      t_CARB_N=0.0D0
+      t_CELL_N=0.0D0
+      t_LIGN_N=0.0D0
+      t_ResidueMulch=0.0D0
+      t_ResidueMulch_N=0.0D0
+      t_ResidueMulch_CNR=0.0D0
+      t_Mulch_Thick=0.0D0
+	C_RespirationOM=0.0D0
+	C_RespirationRoot=0.0D0
        Endif
-          do i=1,NumBp
+	     do i=1,NumBp
 	     n=KXB(i)
 C Cflux is loss of N in mg
 	     if ((CodeW(n).eq.(-7)).or.(CodeW(n).eq.(2))) then
@@ -41,7 +67,8 @@ C Cflux is loss of N in mg
 cccz	        Cflux=Cflux+Q(n)*conc(n,1)*step*14/62
 cccz using a starndard conversion factor "NO3mass_2_Nmass"
 cccz why "mg"??????????????????????????????????????????????????????
-              Cflux=Cflux+Q(n)*conc(n,1)*step*NO3mass_2_Nmass 
+cDT only consider flow out.
+              Cflux=Cflux+min(0.0,Q(n))*conc(n,1)*step*NO3mass_2_Nmass 
            endif
 C for case of downward drainage and a constrant BC 
 C (does not account for upward flow yet)
@@ -53,13 +80,17 @@ cccz why "mg"??????????????????????????????????????????????????????
                  Cflux=Cflux+Q(n)*conc(n,1)*step*NO3mass_2_Nmass  ! only consider chemical out- flux>0
              endif
            endif
-       EndDo
-      !  Do n=1,NumNP
-      !    Rflux=Rflux+Conc(n,1)*Sink(n)*step*NO3mass_2_Nmass*nodeArea(n)
-      !end do 
-	 if (NShoot.eq.1)then
-       Rflux=Rflux +SIncrSink  !CSink(n,1)*Step*nodeArea(n)*14./62.
-       end if
+          EndDo
+     
+csun Calculate the co2 the units are ug
+	   do i=1,NumNP
+C gsink_OM and gSink_root unit is ug CO2 cm-3 air, need to calculate all C_RespirationOM (ugC/domain)		   
+		   C_RespirationOM=C_RespirationOM+gsink_OM(i,1)*Step*12.0*nodeArea(i)
+     !       *soilair(i)/(44.0)
+             C_RespirationRoot=C_RespirationRoot+gSink_root(i,1)*
+     !       Step*12.0*nodeArea(i)*soilair(i)/(44.0)
+	   enddo
+	  
         t=time
         if (Abs(time-tNext(ModNum)).lt.0.001*Step.or.lInput.ne.0) then
            tNext(ModNum)=tNext(ModNum)+1.0
@@ -74,9 +105,7 @@ cccz why "mg"??????????????????????????????????????????????????????
            humusC=0.0
            litterC=0.0
            manureC=0.0
-           CsinkHumus=0.0
-           CsinkLitter=0.0
-           CsinkManure=0.0
+
 	   Sum=0.
            W_Sum=0.
 	   Do n=1,NumEl
@@ -92,9 +121,7 @@ cccz why "mg"??????????????????????????????????????????????????????
              CHumusMean=0.0
              CLitterMean=0.0
              CManureMean=0.0
-             CsinkHuMean=0.0
-             CsinkLiMean=0.0
-             CsinkMaMean=0.0
+
 *         Loop on subelements
              do k=1,NUS-2
                i=KX(n,1)
@@ -117,50 +144,34 @@ cccz Conc is NO3 as ug/cm3
                Sum1=Sum1+AE*(ThNew(i)+ThNew(j)+ThNew(l))/3.
 
 cccz need to present all the N mass based using soil bulk density   
-c  organic N is in ug/g of soil, use bd to get ug/cm3 of soil in order to sum over volume
-               NhumusMean=NhumusMean+AE*(Nh(i)*Blkdn(MatNumN(i))
-     &             +Nh(j)*Blkdn(MatNumN(j))
-     &             +Nh(l)*Blkdn(MatNumN(l)))/3.
-               NLitterMean=NLitterMean+AE*(NL(i)*
-     &                 Blkdn(MatNumN(i))
-     &             +NL(j)*Blkdn(MatNumN(j))
-     &             +NL(l)*Blkdn(MatNumN(l)))/3.
-               NManureMean=NManureMean+AE*(Nm(i)*Blkdn(MatNumN(i))
-     &             +Nm(j)*Blkdn(MatNumN(j))
-     &             +Nm(l)*Blkdn(MatNumN(l)))/3.
+c  organic N is in ug/cm3 of volume, don't need BD order to sum over volume
+               NhumusMean=NhumusMean+AE*(Nh(i)
+     &             +Nh(j)
+     &             +Nh(l))/3.
+               NLitterMean=NLitterMean+AE*(NL(i)
+     &             +NL(j)
+     &             +NL(l))/3.
+               NManureMean=NManureMean+AE*(Nm(i)
+     &             +Nm(j)
+     &             +Nm(l))/3.
      
-               ChumusMean=ChumusMean+AE*(Ch(i)*Blkdn(MatNumN(i))
-     &             +Ch(j)*Blkdn(MatNumN(j))
-     &             +Ch(l)*Blkdn(MatNumN(l)))/3.
-               CLitterMean=CLitterMean+AE*(CL(i)*
-     &                 Blkdn(MatNumN(i))
-     &             +CL(j)*Blkdn(MatNumN(j))
-     &             +CL(l)*Blkdn(MatNumN(l)))/3.
-               CManureMean=CManureMean+AE*(Cm(i)*Blkdn(MatNumN(i))
-     &             +Cm(j)*Blkdn(MatNumN(j))
-     &             +Cm(l)*Blkdn(MatNumN(l)))/3.
+               ChumusMean=ChumusMean+AE*(Ch(i)
+     &             +Ch(j)
+     &             +Ch(l))/3.
+               CLitterMean=CLitterMean+AE*(CL(i)
+     &             +CL(j)
+     &             +CL(l))/3.
+               CManureMean=CManureMean+AE*(Cm(i)
+     &             +Cm(j)
+     &             +Cm(l))/3.
 
-               NAmmoniaMean=NAmmoniaMean+AE*(NNH4(i)*
-     &               Blkdn(MatNumN(i))
-     &             +NNH4(j)*Blkdn(MatNumN(j))
-     &             +NNH4(l)*Blkdn(MatNumN(l)))/3.
-               NDenitrifyMean=NDenitrifyMean+AE*(Denit(i)*
-     &                    Blkdn(MatNumN(i))
-     &             +denit(j)*Blkdn(MatNumN(j))
-     &             +denit(l)*Blkdn(MatNumN(l)))/3.
-                CsinkHuMean=CsinkHuMean+AE*(CSinkHu(i)*
-     &                    Blkdn(MatNumN(i))
-     &             +CSinkHu(j)*Blkdn(MatNumN(j))
-     &             +CSinkHu(l)*Blkdn(MatNumN(l)))/3.              
-                CsinkLiMean=CsinkLiMean+AE*(CSinkLi(i)*
-     &                    Blkdn(MatNumN(i))
-     &             +CSinkLi(j)*Blkdn(MatNumN(j))
-     &             +CSinkLi(l)*Blkdn(MatNumN(l)))/3.                  
-                CsinkMaMean=CsinkMaMean+AE*(CSinkMa(i)*
-     &                    Blkdn(MatNumN(i))
-     &             +CSinkMa(j)*Blkdn(MatNumN(j))
-     &             +CSinkMa(l)*Blkdn(MatNumN(l)))/3.                  
-               
+               NAmmoniaMean=NAmmoniaMean+AE*(NNH4(i)
+     &             +NNH4(j)
+     &             +NNH4(l))/3.
+               NDenitrifyMean=NDenitrifyMean+AE*(Denit(i)
+     &             +denit(j)
+     &             +denit(l))/3.
+       
              Enddo
              Min_N=Min_N+NO3_Mean
              Org_N=Org_N+NhumusMean
@@ -172,19 +183,50 @@ c  organic N is in ug/g of soil, use bd to get ug/cm3 of soil in order to sum ov
              Ammon_N=Ammon_N+NAmmoniaMean
              W_Sum=W_Sum+Sum1
              Denitr=Denitr+NDenitrifyMean
-             CsinkHumus=CsinkHumus+CsinkHuMean
-             CsinkLitter=CsinkLitter+CsinkLiMean
-             CsinkManure=CsinkManure+CsinkMaMean
          Enddo
+                    	   
+Csun Carbon mass balance in soil means the soilC is consistent during time
+	   All_C = humusC + litterC + manureC + C_RespirationOM
+	   
+C calculate total N in surface mulch residue. units are grams
+C in the mulch model so multiply by 1e6
+        t_CARB=0.0D0
+        t_CELL=0.0D0
+        t_LIGN=0.0D0
+        t_CARB_N=0.0D0
+        t_CELL_N=0.0D0
+        t_LIGN_N=0.0D0
+        t_ResidueMulch=0.0D0
+        t_ResidueMulch_N=0.0D0
+        t_ResidueMulch_CNR=0.0D0
+        t_Mulch_Thick=0.0D0	  
 
-         
+        if(BoolMulchApply.eq.1) then
+cccz make sure there is mulch applied
+          do kk=1,SurNodeIndex_M-1
+cccz should be SurNodeIndexM because there may horizontal merging
+            do jj=1,mulchLayer
+              t_CARB=t_CARB+CARB_mass(jj,kk)*1e6
+              t_CELL=t_CELL+CELL_mass(jj,kk)*1e6
+              t_LIGN=t_LIGN+LIGN_mass(jj,kk)*1e6
+              t_CARB_N=t_CARB_N+CARB_N_mass(jj,kk)*1e6
+              t_CELL_N=t_CELL_N+CELL_N_mass(jj,kk)*1e6
+              t_LIGN_N=t_LIGN_N+LIGN_N_mass(jj,kk)*1e6
+            enddo
+          enddo
+          t_ResidueMulch=t_CARB+t_CELL+t_LIGN
+          t_ResidueMulch_N=t_LIGN_N+t_CARB_N+t_CELL_N
+          t_ResidueMulch_CNR=t_ResidueMulch*0.41D0/t_ResidueMulch_N
+          t_Mulch_Thick=mulchThick
+        endif  
+
 C calculate sum of N in all forms in soil
 C factor is appropriate for mg/cm3 Total N is mg per slab (grid width x 1cm)
 C Mineral ad organic N is ug/cm3. Do a summation over the simulation domain - total ug in a plant slab
 C Fact now works while there is a plant 
 
-          fact=1/(0.01*RowSp/100.*EOMult) !m2 of slab
-          fact=fact*10000/1000/1000/1000    !m2 of slab ->ha, ug-->Kg
+          fact=1.0/(0.01*RowSp/100.*EOMult) !m2 of slab
+          fact=fact*10000./1000./1000./1000.    !m2 of slab ->ha, ug-->Kg
           All_N=(Min_N+Org_N+Litter_N+Manure_N
      !           +Ammon_N)*fact
          iday=int(t)
@@ -193,17 +235,20 @@ C Fact now works while there is a plant
           write(91,10) 
      !       time,date,Min_N*fact,
      !       Org_N*fact,humusC*fact,Manure_N*fact,
-     !       manureC*fact, Litter_N*fact,      
-     !       litterC*fact,Ammon_N*fact,
-     !       All_N,W_Sum,Denitr*fact,CFLux*fact
-          
-    !      , RFlux*fact*-1.0,         
-    !        CsinkHumus*fact, CsinkLitter*fact, CsinkManure*fact
-
-
+     !       manureC*fact, Litter_N*fact, 
+     !       litterC*fact, C_RespirationOM*fact,
+     !       C_RespirationRoot*fact,Ammon_N*fact,
+     !       All_N,All_C*fact,W_Sum,Denitr*fact,CFLux*fact,
+     !       t_CARB_N*fact,t_CELL_N*fact,t_LIGN_N*fact,
+     !        t_ResidueMulch_N*fact,
+     !       t_Mulch_Thick,t_ResidueMulch*fact,t_ResidueMulch_CNR 
+     	   
           CFluxPrevious=CFlux
+csun reset a initial value of C_RespirationOM 
+	    C_RespirationOM = 0.0D0
+          C_RespirationRoot = 0.0D0
         endif
-10    Format (1F12.4,',', A12,',', 11(F12.4, ','),F12.4)
+10    Format (1F12.4,',', A12,',', 21(F20.4, ','),F20.4)
       return
       end
 
